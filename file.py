0511fec8a2746b089c93b94d6a7f241e5da516a3
Olarewajurr <orelopequwam@gmail.com>
2025-08-31 18:54:44 +0100

    my commit

--- file.py
+++ file.py
@@ -1,2 +1,16 @@
-value = 1
-print("hello")
+value = int(input("input enter a value "))
+value2 = int(input("enter second value "))
+
+
+
+class MyCustomError(Exception):
+  def __init__()
+  pass
+
+
+if value2 != 0:
+  res = value/value
+else:
+  raise MyCustomError("ola no go gree")
+
+
